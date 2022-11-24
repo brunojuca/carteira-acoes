aff09424812b5731ca621effb8fdf1b94262b0ba
Bruno Juca <brunoolijuca@gmail.com>
2022-11-24 01:20:14 -0300

    Fix issue #4. Filtro em exibir operacoes.

--- main.pl
+++ main.pl
@@ -13,15 +13,33 @@ init :-
 
 
 % compra
-compra(D, M, Y, T, N, P) :-
+compra(D, M, Y, T, P, N) :-
     X is floor(10^2*(0.00025 * N * P + 0.00005 * N * P))/10^2,
     V is N * P + X,
-    assert_operacao(date(D, M, Y), T, "COMPRA", P, N, X, V).
+    assert_operacao(date(D, M, Y), T, 'COMPRA', P, N, X, V).
 
 % venda
-venda(D, M, Y, T, N, P) :-
+venda(D, M, Y, T, P, N) :-
     X is floor(10^2*(0.00025 * N * P + 0.00005 * N * P))/10^2,
     V is N * P - X,
     N1 is -N,
     V1 is -V,
-    assert_operacao(date(D, M, Y), T, "VENDA", P, N1, X, V1).
+    assert_operacao(date(D, M, Y), T, 'VENDA', P, N1, X, V1).
+
+exibir_operacoes :-
+format('\n|~t~a~t~13||~t~a~t~9+|~t~a~t~11+|~t~a~t~17+|~t~a~t~13+|~t~a~t~8+|~t~a~t~14+|~n', 
+    ['DATA', 'TICKET', 'OPERAÇÃO', 'PREÇO UNITÁRIO', 'QUANTIDADE', 'TAXAS', 'CUSTO TOTAL']),
+format("|~`-t~85||~n"),
+forall(operacao(date(D, M, Y), T, Z, P, N, X, V), 
+    format('| ~`0t~a~4+/~`0t~a~3+/~t~a~t~6+|~t~a~t~9+|~t~a~t~11+|~t~2f~16+ |~t~d~13+ |~t~2f~8+ |~t~2f~14+ |~n', 
+        [D, M, Y, T, Z, P, N, X, V])),
+write('\n').
+
+exibir_operacoes(T, Z, Y) :-
+format('\n|~t~a~t~13||~t~a~t~9+|~t~a~t~11+|~t~a~t~17+|~t~a~t~13+|~t~a~t~8+|~t~a~t~14+|~n', 
+    ['DATA', 'TICKET', 'OPERAÇÃO', 'PREÇO UNITÁRIO', 'QUANTIDADE', 'TAXAS', 'CUSTO TOTAL']),
+format("|~`-t~85||~n"),
+forall(operacao(date(D, M, Y), T, Z, P, N, X, V), 
+    format('| ~`0t~a~4+/~`0t~a~3+/~t~a~t~6+|~t~a~t~9+|~t~a~t~11+|~t~2f~16+ |~t~d~13+ |~t~2f~8+ |~t~2f~14+ |~n', 
+        [D, M, Y, T, Z, P, N, X, V])),
+write('\n').
