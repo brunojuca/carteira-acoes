968855f61c3d49562848923b6d1b2dc862626e04
Bruno Juca <brunoolijuca@gmail.com>
2022-11-27 13:24:14 -0300

    Fix #3. Exibindo lucro por ano.

--- main.pl
+++ main.pl
@@ -78,3 +78,37 @@ carteira :-
     format("|~`-t~53||~n"),
     maplist(calcula_acao, Ts),
     write('\n').
+
+sum_lucro_carteira((T-'VENDA'-_-N-X-V-_), (_-_-Pm-Na-_-Va-La), Result) :-
+    Vt is Va + V,
+    Nt is Na + N,
+    Lt is La + (Pm*N-V),
+    (Nt =:= 0 ->
+        Pmn = 0,
+        Vtn = 0
+        ;
+        Pmn = Pm,
+        Vtn = Vt),
+    Result = (T-'COMPRA'-Pmn-Nt-X-Vtn-Lt).
+
+sum_lucro_carteira((T-'COMPRA'-P-N-X-V-_), (_-_-_-Na-_-Va-La), Result) :-
+    Vt is Va + V,
+    Nt is Na + N,
+    Pmn is (P*N + Va)/Nt,
+    Result = (T-'COMPRA'-Pmn-Nt-X-Vt-La).
+
+calcula_lucro(Y, T) :-
+    bagof((T-Z-P-N-X-V-0), D^M^Y^(operacao(date(D, M, Y), T, Z, P, N, X, V)), Ts),
+    foldl(sum_lucro_carteira, Ts, (T-_-0-0-0-0-0), S), !, % corte pois os casos de sum_lucro_carteira são excludentes
+    (_-_-_-_-_-_-L) = S,
+    format('|~t~a~t~8||~t~2f~9+ |~n', 
+    [T, L]).
+
+
+lucro(Y) :-
+    setof((T), D^M^Y^P^N^X^V^(operacao(date(D, M, Y), T, 'VENDA', P, N, X, V)), Ts),
+    format('\n|~t~a~t~8||~t~a~t~10+|~n', 
+    ['ACÃO', 'LUCRO']),
+    format("|~`-t~18||~n"),
+    maplist(calcula_lucro(Y), Ts),
+    write('\n').
